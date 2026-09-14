031cf9850346ce4fa3bc096d4dea5d0e31bbdd9b
sabatinySingularity <sabatiny@singularitysystems.net>
2026-09-14 18:53:52 -0300

    Refactor, make constants and decoup the animation in minor methods steps

--- Assets/Project/Script/Views/BoardView.cs
+++ Assets/Project/Script/Views/BoardView.cs
@@ -11,6 +11,16 @@ namespace Gazeus.DesafioMatch3.Views
     {
         public event Action<int, int> TileClicked;
 
+        #region Constants
+        private const float TILE_SPACING = 1.3f;
+        private const float TILE_APPEAR_INTERVAL_DURATION = 0.1f;
+        private const float ANIMATION_DURATION = 0.5f;
+        private const float DESTROY_ANIMATION_DELAY = 0.2f;
+        private const float SCALE_PHASE_RATIO = 0.7f;
+        private const float PUNCH_PHASE_RATIO = 0.3f;
+        private static readonly Vector3 PUNCH_STRENGTH = new(0.2f, 0.2f, 0.2f);
+        #endregion
+
         [SerializeField]
         private Transform _boardContainer;
 
@@ -25,46 +35,20 @@ namespace Gazeus.DesafioMatch3.Views
 
         public void CreateBoard(List<List<Tile>> board)
         {
-            _tiles = new GameObject[board.Count][];
-            _tileSpots = new TileSpotView[board.Count][];
+            int rowCount = board.Count;
+            int colCount = board[0].Count;
 
-            for (int y = 0; y < board.Count; y++)
+            _tiles = new GameObject[rowCount][];
+            _tileSpots = new TileSpotView[rowCount][];
+
+            for (int y = 0; y < rowCount; y++)
             {
-                _tiles[y] = new GameObject[board[0].Count];
-                _tileSpots[y] = new TileSpotView[board[0].Count];
+                _tiles[y] = new GameObject[colCount];
+                _tileSpots[y] = new TileSpotView[colCount];
 
-                for (int x = 0; x < board[0].Count; x++)
+                for (int x = 0; x < colCount; x++)
                 {
-                    GameObject spot = new($"Tile Spot ({x}-{y})", typeof(TileSpotView));
-
-                    spot.transform.position = new Vector3(x * 1.3f, y * 1.3f, 0);
-                    spot.transform.SetParent(_boardContainer);
-
-                    if (spot.TryGetComponent(out TileSpotView tileSpot))
-                    {
-                        tileSpot.SetPosition(x, y);
-                        tileSpot.Clicked += TileSpot_Clicked;
-
-                        _tileSpots[y][x] = tileSpot;
-
-                        int tileTypeIndex = board[y][x].Type;
-
-                        if (tileTypeIndex < 0)
-                        {
-                            Debug.LogError(
-                                $"[Board View] Tile type should not be under zero, tileTypeIndex is {tileTypeIndex} x:{x} and y:{y}"
-                            );
-                        }
-                        else
-                        {
-                            SpriteRenderer tile = Instantiate(_tilePrefab);
-
-                            tile.transform.localScale = Vector3.zero;
-                            tile.color = _tilePrefabRepository.TileTypes[tileTypeIndex];
-                            tileSpot.SetTile(tile.gameObject);
-                            _tiles[y][x] = tile.gameObject;
-                        }
-                    }
+                    CreateTileSpotAndTile(board, x, y);
                 }
             }
         }
@@ -72,64 +56,55 @@ namespace Gazeus.DesafioMatch3.Views
         public Tween CreateTile(List<AddedTileInfo> addedTiles)
         {
             Sequence sequence = DOTween.Sequence();
-            float animationDuration = 0.5f;
 
-            for (int i = 0; i < addedTiles.Count; i++)
+            foreach (var addedTile in addedTiles)
             {
-                AddedTileInfo addedTileInfo = addedTiles[i];
-                Vector2Int position = addedTileInfo.Position;
-                TileSpotView tileSpot = _tileSpots[position.y][position.x];
+                Vector2Int pos = addedTile.Position;
                 SpriteRenderer tile = Instantiate(_tilePrefab);
 
-                tile.color = _tilePrefabRepository.TileTypes[addedTiles[i].Type];
-                tileSpot.SetTile(tile.gameObject);
-                _tiles[position.y][position.x] = tile.gameObject;
-                tile.transform.localScale = Vector2.zero;
-                sequence.Join(tile.transform.DOScale(1.0f, animationDuration));
+                tile.color = _tilePrefabRepository.TileTypes[addedTile.Type];
+                tile.transform.localScale = Vector3.zero;
+
+                _tileSpots[pos.y][pos.x].SetTile(tile.gameObject);
+                _tiles[pos.y][pos.x] = tile.gameObject;
+
+                sequence.Join(tile.transform.DOScale(Vector3.one, ANIMATION_DURATION));
             }
 
             return sequence;
         }
 
-        public Tween DestroyTiles(List<Vector2Int> matchedPosition)
+        public Tween DestroyTiles(List<Vector2Int> matchedPositions)
         {
-            for (int i = 0; i < matchedPosition.Count; i++)
+            foreach (var pos in matchedPositions)
             {
-                Vector2Int position = matchedPosition[i];
-                Destroy(_tiles[position.y][position.x]);
-                _tiles[position.y][position.x] = null;
+                if (_tiles[pos.y][pos.x] == null)
+                {
+                    continue;
+                }
+
+                Destroy(_tiles[pos.y][pos.x]);
+                _tiles[pos.y][pos.x] = null;
             }
 
-            return DOVirtual.DelayedCall(0.2f, () => { });
+            return DOVirtual.DelayedCall(DESTROY_ANIMATION_DELAY, () => { });
         }
 
         public Tween MoveTiles(List<MovedTileInfo> movedTiles)
         {
-            GameObject[][] tiles = new GameObject[_tiles.Length][];
-            for (int y = 0; y < _tiles.Length; y++)
-            {
-                tiles[y] = new GameObject[_tiles[y].Length];
-                for (int x = 0; x < _tiles[y].Length; x++)
-                {
-                    tiles[y][x] = _tiles[y][x];
-                }
-            }
-
+            GameObject[][] nextTilesState = CloneTilesGrid();
             Sequence sequence = DOTween.Sequence();
-            for (int i = 0; i < movedTiles.Count; i++)
-            {
-                MovedTileInfo movedTileInfo = movedTiles[i];
 
-                Vector2Int from = movedTileInfo.From;
-                Vector2Int to = movedTileInfo.To;
+            foreach (var movedTile in movedTiles)
+            {
+                Vector2Int from = movedTile.From;
+                Vector2Int to = movedTile.To;
 
                 sequence.Join(_tileSpots[to.y][to.x].AnimatedSetTile(_tiles[from.y][from.x]));
-
-                tiles[to.y][to.x] = _tiles[from.y][from.x];
+                nextTilesState[to.y][to.x] = _tiles[from.y][from.x];
             }
 
-            _tiles = tiles;
-
+            _tiles = nextTilesState;
             return sequence;
         }
 
@@ -140,29 +115,20 @@ namespace Gazeus.DesafioMatch3.Views
             sequence.Join(_tileSpots[toY][toX].AnimatedSetTile(_tiles[fromY][fromX]));
 
             (_tiles[toY][toX], _tiles[fromY][fromX]) = (_tiles[fromY][fromX], _tiles[toY][toX]);
-
             return sequence;
         }
 
-
-        /// <summary>
-        /// Faz o tabuleiro aparecer em efeito cascata
-        /// </summary>
-        /// <returns></returns>
         public Tween MakeAllTilesPopUp()
         {
             Sequence sequence = DOTween.Sequence();
-            float intervalDuration = 0.1f;
-            float animationDuration = 0.5f;
 
-            if (_tiles == null || _tiles.Length == 0)
+            if (IsBoardInvalid())
             {
                 return sequence;
             }
 
             int numRows = _tiles.Length;
             int numCols = _tiles[0].Length;
-
             bool[,] processed = new bool[numRows, numCols];
 
             float currentTime = 0f;
@@ -170,56 +136,129 @@ namespace Gazeus.DesafioMatch3.Views
 
             for (int step = 0; step < maxSteps; step++)
             {
-                int targetCol = numCols - 1 - step;
-                int targetRow = step;
-                bool addedAnyInThisStep = false;
-
-                if (targetCol >= 0)
+                if (EnqueueStepAnimations(step, numRows, numCols, processed, sequence, currentTime))
                 {
-                    for (int r = 0; r < numRows; r++)
-                    {
-                        if (r < numRows && targetCol < _tiles[r].Length && !processed[r, targetCol])
-                        {
-                            AnimateTile(
-                                _tiles[r][targetCol],
-                                sequence,
-                                currentTime,
-                                animationDuration
-                            );
-                            processed[r, targetCol] = true;
-                            addedAnyInThisStep = true;
-                        }
-                    }
-                }
-
-                if (targetRow < numRows && _tiles[targetRow] != null)
-                {
-                    for (int c = 0; c < _tiles[targetRow].Length; c++)
-                    {
-                        if (!processed[targetRow, c])
-                        {
-                            AnimateTile(
-                                _tiles[targetRow][c],
-                                sequence,
-                                currentTime,
-                                animationDuration
-                            );
-                            processed[targetRow, c] = true;
-                            addedAnyInThisStep = true;
-                        }
-                    }
-                }
-
-                if (addedAnyInThisStep)
-                {
-                    currentTime += intervalDuration;
+                    currentTime += TILE_APPEAR_INTERVAL_DURATION;
                 }
             }
 
             return sequence;
         }
 
-        private void AnimateTile(GameObject tile, Sequence sequence, float atTime, float duration)
+        private void CreateTileSpotAndTile(List<List<Tile>> board, int x, int y)
+        {
+            GameObject spotGo = new($"Tile Spot ({x}-{y})", typeof(TileSpotView));
+            spotGo.transform.SetParent(_boardContainer);
+            spotGo.transform.position = new Vector3(x * TILE_SPACING, y * TILE_SPACING, 0f);
+
+            if (!spotGo.TryGetComponent(out TileSpotView tileSpot))
+            {
+                return;
+            }
+
+            tileSpot.SetPosition(x, y);
+            tileSpot.Clicked += TileSpot_Clicked;
+            _tileSpots[y][x] = tileSpot;
+
+            int tileTypeIndex = board[y][x].Type;
+            if (tileTypeIndex < 0)
+            {
+                Debug.LogError($"[BoardView] Invalid tile type {tileTypeIndex} at x:{x}, y:{y}");
+                return;
+            }
+
+            SpriteRenderer tile = Instantiate(_tilePrefab);
+            tile.transform.localScale = Vector3.zero;
+            tile.color = _tilePrefabRepository.TileTypes[tileTypeIndex];
+
+            tileSpot.SetTile(tile.gameObject);
+            _tiles[y][x] = tile.gameObject;
+        }
+
+        private GameObject[][] CloneTilesGrid()
+        {
+            GameObject[][] clone = new GameObject[_tiles.Length][];
+            for (int y = 0; y < _tiles.Length; y++)
+            {
+                clone[y] = new GameObject[_tiles[y].Length];
+                Array.Copy(_tiles[y], clone[y], _tiles[y].Length);
+            }
+            return clone;
+        }
+
+        private bool IsBoardInvalid() => _tiles == null || _tiles.Length == 0 || _tiles[0] == null;
+
+        private bool EnqueueStepAnimations(
+            int step,
+            int numRows,
+            int numCols,
+            bool[,] processed,
+            Sequence sequence,
+            float currentTime
+        )
+        {
+            int targetCol = numCols - 1 - step;
+            int targetRow = step;
+
+            bool columnAnimated =
+                targetCol >= 0
+                && EnqueueColumnAnimations(targetCol, numRows, processed, sequence, currentTime);
+            bool rowAnimated =
+                targetRow < numRows
+                && EnqueueRowAnimations(targetRow, processed, sequence, currentTime);
+
+            return columnAnimated || rowAnimated;
+        }
+
+        private bool EnqueueColumnAnimations(
+            int col,
+            int numRows,
+            bool[,] processed,
+            Sequence sequence,
+            float currentTime
+        )
+        {
+            bool hasAnimatedAny = false;
+
+            for (int r = 0; r < numRows; r++)
+            {
+                if (_tiles[r] == null || col >= _tiles[r].Length || processed[r, col])
+                    continue;
+
+                AnimateTile(_tiles[r][col], sequence, currentTime);
+                processed[r, col] = true;
+                hasAnimatedAny = true;
+            }
+
+            return hasAnimatedAny;
+        }
+
+        private bool EnqueueRowAnimations(
+            int row,
+            bool[,] processed,
+            Sequence sequence,
+            float currentTime
+        )
+        {
+            if (_tiles[row] == null)
+                return false;
+
+            bool hasAnimatedAny = false;
+
+            for (int c = 0; c < _tiles[row].Length; c++)
+            {
+                if (processed[row, c])
+                    continue;
+
+                AnimateTile(_tiles[row][c], sequence, currentTime);
+                processed[row, c] = true;
+                hasAnimatedAny = true;
+            }
+
+            return hasAnimatedAny;
+        }
+
+        private void AnimateTile(GameObject tile, Sequence mainSequence, float atTime)
         {
             if (tile == null)
             {
@@ -229,26 +268,23 @@ namespace Gazeus.DesafioMatch3.Views
             tile.transform.localScale = Vector3.zero;
 
             Sequence tileSequence = DOTween.Sequence();
-
-            tileSequence.Append(tile.transform.DOScale(Vector3.one, duration * 0.7f));
-
+            tileSequence.Append(
+                tile.transform.DOScale(Vector3.one, ANIMATION_DURATION * SCALE_PHASE_RATIO)
+            );
             tileSequence.Append(
                 tile.transform.DOPunchScale(
-                    new Vector3(0.2f, 0.2f, 0.2f),
-                    duration * 0.3f,
-                    vibrato: 1,
-                    elasticity: 0.5f
+                    PUNCH_STRENGTH,
+                    ANIMATION_DURATION * PUNCH_PHASE_RATIO,
+                    1,
+                    0.5f
                 )
             );
 
-            sequence.Insert(atTime, tileSequence);
+            mainSequence.Insert(atTime, tileSequence);
         }
 
         #region Events
-        private void TileSpot_Clicked(int x, int y)
-        {
-            TileClicked(x, y);
-        }
+        private void TileSpot_Clicked(int x, int y) => TileClicked?.Invoke(x, y);
         #endregion
     }
 }
