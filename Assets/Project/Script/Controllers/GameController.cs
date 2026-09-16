b5b6afecb8491170959602b2d4758c9fb2df7ea1
sabatinySingularity <sabatiny@singularitysystems.net>
2026-09-16 18:32:58 -0300

    Adding basic capacity to avoid tweens resizing

--- Assets/Project/Script/Controllers/GameController.cs
+++ Assets/Project/Script/Controllers/GameController.cs
@@ -32,6 +32,7 @@ namespace Gazeus.DesafioMatch3.Controllers
         #region Unity
         private void Awake()
         {
+            DOTween.SetTweensCapacity(500, 125);
             _gameService = new GameService();
             _scoreController = new ScoreController();
             _boardView.TileClicked += OnTileClick;
